16175bb36cc68f65526675110e2fb8f6329f99d5
Paulo Eduardo <paulo.es@live.com>
2019-07-01 19:01:28 -0300

    creating initial api

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("combined"));
+app.use(cors("*"));
+app.use(bodyParser.json());
+
+app.use("/api/v1", require("./v1/router"));
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
