4d66e829043341b5a19ae15c189928c5a9e3e2e4
Paulo Eduardo <paulo.es@live.com>
2019-07-02 08:22:03 -0300

    changing authentication to basic auth

--- src/v1/router.js
+++ src/v1/router.js
@@ -1,18 +1,21 @@
 const express = require('express')
+const basicAuth = require('express-basic-auth')
 
 const booksController = require('./controllers/books')
-
-const auth = require('./controllers/auth')
 
 const routes = new express.Router()
 
 routes.get('/', (req, res) => res.status(200).json({ message: 'server is up' }))
 
-routes.post('/login', auth.signinAuthentication)
+// routes.post('/login', auth.signinAuthentication)
 
 routes.get('/book', booksController.index)
 routes.get('/report', booksController.listVotes)
-routes.post('/book/:id', auth.requireAuth, booksController.storeVote)
+routes.post(
+  '/book/:id',
+  basicAuth({ users: { admin: 'admin' } }),
+  booksController.storeVote
+)
 routes.get('/truncateVotes', booksController.truncateVotes)
 
 module.exports = routes
